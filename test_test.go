9930847ef213f06f12cff0d5fae580707b6dd029
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-08-28 22:20:31 +0100

    test: request_id ctx

--- test_test.go
+++ test_test.go
@@ -26,7 +26,7 @@ func TestTest(t *testing.T) {
 
 	client := clearbank.NewClient("token", mockSigner, clearbank.WithHTTPClient(mockHttpClient), clearbank.WithLogger(logger))
 
-	ctx := context.TODO()
+	ctx := clearbank.RequestIdContext(context.TODO(), "123")
 
 	mockSigner.On("Sign", ctx, mock.Anything).Return([]byte("signed"), nil).Once()
 
@@ -41,6 +41,8 @@ func TestTest(t *testing.T) {
 	require.Contains(t, hook.Entries[1].Data, "http.response.status_code")
 	require.Contains(t, hook.Entries[1].Data, "http.response.body.content")
 	require.Contains(t, hook.Entries[1].Data, "http.response.headers")
+
+	assert.Equal(t, "123", hook.Entries[0].Data["http.request.headers.request_id"])
 }
 
 func TestFailedSign(t *testing.T) {
