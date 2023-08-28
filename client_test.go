9aac89fa5e92f8e09ac0a2baab937cf72f53ca22
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-08-28 22:33:49 +0100

    test: options

--- client_test.go
+++ client_test.go
@@ -4,8 +4,20 @@ import (
 	"context"
 	"testing"
 
+	logrustest "github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNewClient(t *testing.T) {
+	httpClient := NewMockHttpClient(t)
+	logger, _ := logrustest.NewNullLogger()
+
+	client := NewClient("", nil, WithBaseURL("https://c.om"), WithHTTPClient(httpClient), WithLogger(logger))
+
+	assert.Equal(t, "https://c.om", client.baseURL)
+	assert.Same(t, httpClient, client.httpClient)
+	assert.Same(t, logger, client.logger)
+}
 
 func TestRequestIdFromContext(t *testing.T) {
 	assert.Equal(t, "", RequestIdFromContext(nil)) //lint:ignore SA1012 this is a test
