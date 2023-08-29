283c635b483d620dc19cc055bfb0fc5c0056867b
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-08-29 21:04:59 +0100

    feat: stream

---
+++ stream/request.go
@@ -0,0 +1,43 @@
+package stream
+
+import (
+	"encoding/json"
+
+	"github.com/brokeyourbike/clearbank-api-client-go"
+)
+
+type SubscriptionRequest struct {
+	Type    string `json:"Type"`
+	Version uint64 `json:"Version"`
+	Payload struct {
+		CurrencyPair string `json:"CurrencyPair"`
+	} `json:"Payload"`
+}
+
+func NewSubscriptionRequest(symbol string) SubscriptionRequest {
+	req := SubscriptionRequest{Type: "SubscriptionRequest", Version: 1}
+	req.Payload.CurrencyPair = symbol
+	return req
+}
+
+type MarketMessage struct {
+	Type    string          `json:"Type"`
+	Version uint64          `json:"Version"`
+	Payload json.RawMessage `json:"Payload"`
+}
+
+type HeartbeatPayload struct {
+	Time clearbank.Time `json:"Time"`
+}
+
+type MarketDataPayload struct {
+	Symbol      string         `json:"Symbol"`
+	Currency    string         `json:"Currency"`
+	ValueDate   clearbank.Time `json:"ValueDate"`
+	Type        string         `json:"Type"`
+	SendingTime string         `json:"SendingTime"`
+	Entries     []struct {
+		Size  float64 `json:"Size"`
+		Price float64 `json:"Price"`
+	} `json:"Entries"`
+}
