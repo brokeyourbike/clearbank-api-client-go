42cacce3bac9af2bfb9160e556b63c4d11ba7d0b
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-10-07 15:03:10 +0100

    test: account

--- accounts_test.go
+++ accounts_test.go
@@ -9,13 +9,33 @@ import (
 	"testing"
 
 	"github.com/brokeyourbike/clearbank-api-client-go"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	mock "github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
+//go:embed testdata/account-success.json
+var accountSuccess []byte
+
 //go:embed testdata/accounts-success.json
 var accountsSuccess []byte
+
+func TestFetchAccount(t *testing.T) {
+	mockHttpClient := clearbank.NewMockHttpClient(t)
+	client := clearbank.NewClient("token", nil, clearbank.WithHTTPClient(mockHttpClient))
+
+	resp := &http.Response{StatusCode: 200, Body: io.NopCloser(bytes.NewReader(accountSuccess))}
+	mockHttpClient.On("Do", mock.AnythingOfType("*http.Request")).Return(resp, nil).Once()
+
+	accountID := uuid.MustParse("a85002e3-0116-4b14-b7fa-427e60f4f6bc")
+
+	got, err := client.FetchAccount(context.TODO(), accountID)
+	require.NoError(t, err)
+	assert.Equal(t, accountID, got.Account.ID)
+	assert.Len(t, got.Account.Balances, 1)
+	assert.Len(t, got.Account.Currency, 1)
+}
 
 func TestFetchAccounts(t *testing.T) {
 	mockHttpClient := clearbank.NewMockHttpClient(t)
