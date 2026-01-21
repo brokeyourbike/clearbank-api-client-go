28659dea89002af4de74182535d34c4342ca2333
Ivan Stasiuk <brokeyourbike@gmail.com>
2026-01-21 01:14:15 +0000

    test: err case

---
+++ bacs_test.go
@@ -0,0 +1,27 @@
+package clearbank_test
+
+import (
+	"testing"
+
+	"github.com/brokeyourbike/clearbank-api-client-go"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFetchAccountMandates_RequestErr(t *testing.T) {
+	mockHttpClient := clearbank.NewMockHttpClient(t)
+	client := clearbank.NewClient("token", nil, clearbank.WithHTTPClient(mockHttpClient))
+
+	_, err := client.FetchAccountMandates(nil, uuid.New(), 0, 0) //lint:ignore SA1012 testing failure
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to create request")
+}
+
+func TestFetchVirtualAccountMandates_RequestErr(t *testing.T) {
+	mockHttpClient := clearbank.NewMockHttpClient(t)
+	client := clearbank.NewClient("token", nil, clearbank.WithHTTPClient(mockHttpClient))
+
+	_, err := client.FetchVirtualAccountMandates(nil, uuid.New(), uuid.New(), 0, 0) //lint:ignore SA1012 testing failure
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to create request")
+}
